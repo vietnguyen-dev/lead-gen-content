b7a0b073aa9827febfa9fba18bedea3b5cbc8c69
Tom <keith.chen.dev@gmail.com>
2026-04-17 21:08:31 -0700

    being able to import

---
+++ index.ts
@@ -0,0 +1,9 @@
+export { default as testTemplate } from "./property-wholesaling/test-template";
+export type {
+  iAbout,
+  iContact,
+  iFAQ,
+  iHome,
+  iOfferForm,
+  iOfferPage,
+} from "./property-wholesaling/test-template";
--- property-wholesaling/test-template/index.ts
+++ property-wholesaling/test-template/index.ts
@@ -1,8 +1,15 @@
-export { buymyhousepdx } from './buymyhousepdx.com';
-export { portlandpropertybuyers } from './portlandpropertybuyers.com';
+import { buymyhousepdx } from "./buymyhousepdx.com";
+import { portlandpropertybuyers } from "./portlandpropertybuyers.com";
 
-export type { iAbout } from './iAbout';
-export type { iContact } from './iContact';
-export type { iFAQ } from './iFAQ';
-export type { iHome } from './iHome';
-export type { iOfferForm, iOfferPage } from './iGetOffer';
+const testTemplate = {
+  buymyhousepdx: buymyhousepdx,
+  portlandpropertybuyers: portlandpropertybuyers,
+};
+
+export type { iAbout } from "./iAbout";
+export type { iContact } from "./iContact";
+export type { iFAQ } from "./iFAQ";
+export type { iHome } from "./iHome";
+export type { iOfferForm, iOfferPage } from "./iGetOffer";
+
+export default testTemplate;
